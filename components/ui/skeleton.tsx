0d85cf5287a827a747a1968085aaf1dcc31cfdb6
devden7 <devdenn7@gmail.com>
2024-01-27 08:04:37 +0700

    fix error component library shadcn

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,4 +1,5 @@
-import { cn } from "@/lib/utils"
+import React from "react";
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
@@ -6,10 +7,13 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-slate-100 dark:bg-slate-800", className)}
+      className={cn(
+        "animate-pulse rounded-md bg-slate-100 dark:bg-slate-800",
+        className
+      )}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
